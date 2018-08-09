c0eedf39a4fc720ec2d4ef72565495c7315da5c0
Xinru Chen <tonytonytony0724@gmail.com>
2018-08-09 15:10:45 +1200

    index coordinated

--- orderConvertor.py
+++ orderConvertor.py
@@ -24,7 +24,7 @@ args = parser.parse_args()
 csv_file = args.file
 
 print("debug")
-new_csv_file = "C:\\Users\\OEM\\Desktop\\nzkwRebuild\\databaseTransfer\\newOrder.csv" # need to be arguments
+new_csv_file = "C:\\Users\\OEM\\Desktop\\nzkwRebuild\\newOrder.csv" # need to be arguments
 
 included_cols = [2, 3, 5, 8]
 i = 0
@@ -32,12 +32,32 @@ i = 0
 # open csv file
 with open(csv_file, 'r') as csvfile:
     # 43 columns : product code -37
-    headerLine = "Order Reference,Order Grant Total,Customer First Name,Customer Last Name,Customer Email,Billing Address Line 1,Billing Address Line 2,Billing Address Line 3,Billing Address Town,Billing Address Post Code,Billing Address Region,Billing Address Country,Shipping Address Same As Billing,Shipping Address Line 1,Shipping Address Line 2,Shipping Address Line 3,Shipping Address Town,Shipping Address Post Code,Shipping Address Region,Shipping Address Country,Shipping Method,Requested Shipping Date,Order Shipping Amount,Order Shipping Description,Order Shipping Tax Percent,Order Shipping Tax Amount,Payment Method,Is Payment Received,Payment Reference,Is Manually Approved,Payment Terms,Public Notes,Private Notes,Is Tax Applicable,Order Discount Amount,Prices Tax Inclusive,Label List,Product Code,Quantity,Unit Price,Tax Code,Tax Percent,Tax Line Amount,Line Note"
+    headerLineTradevine = "Order Reference,Order Grant Total,Customer First Name,Customer Last Name,Customer Email,Billing Address Line 1,Billing Address Line 2,Billing Address Line 3,Billing Address Town,Billing Address Post Code,Billing Address Region,Billing Address Country,Shipping Address Same As Billing,Shipping Address Line 1,Shipping Address Line 2,Shipping Address Line 3,Shipping Address Town,Shipping Address Post Code,Shipping Address Region,Shipping Address Country,Shipping Method,Requested Shipping Date,Order Shipping Amount,Order Shipping Description,Order Shipping Tax Percent,Order Shipping Tax Amount,Payment Method,Is Payment Received,Payment Reference,Is Manually Approved,Payment Terms,Public Notes,Private Notes,Is Tax Applicable,Order Discount Amount,Prices Tax Inclusive,Label List,Product Code,Quantity,Unit Price,Tax Code,Tax Percent,Tax Line Amount,Line Note"
+    elements = headerLineTradevine.split(",")
+    i = 0
+    for element in elements:
+        element += "[" + str(i) + "]"
+        i += 1
+        print(element)
 
+    headerLineZencart = "Order ID,Customer Email,First Name,Last Name,Company,Delivery Street,Delivery Suburb,Delivery City,Delivery State,Delivery Post Code,Delivery Country,Ship Dest Type,Shipping Method,Shipping Total,Customers Telephone,Order Total,Order Date,Order Notes,Order Tax,Order Discount,Payment Method"
+    print("-" * 40)
+
+    elementsB = headerLineZencart.split(",")
+    j = 0
+    for element in elementsB:
+        element += "[" + str(j) + "]"
+        j += 1
+        print(element)
+
+
+    with open(new_csv_file, "a") as csvFile:
+        writeToCSV(csvFile, headerLineTradevine)
 
     # get number of columns
     for line in csvfile.readlines():
-        array = line.split(',')
+
+        """array = line.split(',')
         firstName, lastName, emailAddress, phoneNumber = setColumn(included_cols, array)
 
         newCSVline = emailAddress + "," + firstName + "," + lastName + ",,,,,,,,,,,,,,,,"
@@ -47,7 +67,11 @@ with open(csv_file, 'r') as csvfile:
             break
 
         with open(new_csv_file, "a") as csvFile:
-            writeToCSV(csvFile, newCSVline)
+            writeToCSV(csvFile, newCSVline)"""
+        infoArray = line.split(',') # todo: get array of 43 items all empty and fill them with necessary info
+
+
+        pass
 
 
 
