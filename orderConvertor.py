fffac9bde1ef7381cd27cc3e36d1a6165ab96c93
Xinru Chen <tonytonytony0724@gmail.com>
2018-10-08 10:53:37 +1300

    bank transfer to EFPOS

--- orderConvertor.py
+++ orderConvertor.py
@@ -144,7 +144,7 @@ def startConvertingCSV(csv_file):
             if paymentMethod == 'PayPal' or paymentMethod == 'dps':
                 arrayTradevine[26] = "PayPal"
             else:
-                arrayTradevine[26] = "Bank Transfer"
+                arrayTradevine[26] = "EFPOS"
 
             quantity = zencartArray[21]
             arrayTradevine[38] = quantity
@@ -220,7 +220,7 @@ def startConvertingCSV(csv_file):
                 elif paymentMethod == 'dps':
                     arrayTradevine[26] = 'Credit Card'
                 else:
-                    arrayTradevine[26] = "Bank Transfer"
+                    arrayTradevine[26] = "EFPOS"
 
 
     #----------------------------------------------------------------------------------------------------------
