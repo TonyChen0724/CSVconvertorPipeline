3e3c691c80b6b39ed8af5093489f472aa3fce31d
Xinru Chen <tonytonytony0724@gmail.com>
2018-10-25 17:19:30 +1300

    some broken method cloaked

--- productCodeConvertor.py
+++ productCodeConvertor.py
@@ -34,14 +34,14 @@ def product_code_convertor(product_code_template, product_attributes):
             append_value = npt_convertor(product_attribute_item + " ")
         elif attribute_type == 'Oil Cooler Options':
             append_value = oil_cooler_option_convertor(product_attribute_item)
-        elif attribute_type == 'Dimension':
-            append_value = dimension_convertor(product_attribute_item)
+        # elif attribute_type == 'Dimension':
+        #     append_value = dimension_convertor(product_attribute_item)
         elif attribute_type == 'Banjo sizes':
             append_value = banjoSize_convertor(product_attribute_item)
         elif attribute_type == 'Hose Dimensions':
             append_value = hose_dimension_convertor(product_attribute_item)
-        elif attribute_type == 'Options-Metric':
-            append_value = options_metric_convertor(product_attribute_item)
+        # elif attribute_type == 'Options-Metric':
+        #     append_value = options_metric_convertor(product_attribute_item)
         elif attribute_type == 'Left / Right':
             append_value = leftRight_convertor(product_attribute_item)
         elif attribute_type == 'Gloves sizes':
